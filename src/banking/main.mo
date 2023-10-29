70aee1350c494d26214a4a87fdda5f096d939a57
Kashish Sharma <kashish6011@gmail.com>
2023-10-30 00:17:55 +0530

    initialisation

--- src/banking/main.mo
+++ src/banking/main.mo
@@ -1,5 +1,46 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Int "mo:base/Int";
+import Float "mo:base/Float";
+
+actor banking {
+  stable var currentValue: Float = 300;
+   currentValue := 100;
+
+  Debug.print( debug_show(currentValue));
+  stable var startTime = Time.now();
+ startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  public func topUp(amount: Float){
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+    
+
+    public func withdraw(amount: Float)
+    {
+      let temp: Float = currentValue - amount;
+      if(temp >= 0) {
+         currentValue -= amount;
+         Debug.print(debug_show(currentValue));
+      }
+      else{
+          Debug.print("Amount is too Large! Current Value is less than zero.");
+      };
+     
+    };
+
+    public query func checkBalance(): async Float {
+      return currentValue;
+    };
+
+    
+    public func compound() {
+        let currentTime = Time.now();
+        let timeElapseNS = currentTime - startTime;
+        let timeElapseS = timeElapseNS / 1000000000;
+        currentValue := currentValue * (1.01 ** Float.fromInt(timeElapseS));
+        startTime := currentTime;
+    }
+}
--- src/banking_assets/src/index.js
+++ src/banking_assets/src/index.js
@@ -1,19 +1,39 @@
 import { banking } from "../../declarations/banking";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await banking.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function() {
+   update();
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  // console.log("Submitted");
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true)
+
+if (document.getElementById("input-amount").value.length != 0) {
+  await banking.topUp(inputAmount);
+}
+
+if (document.getElementById("withdrawal-amount").value.length != 0){
+  await banking.withdraw(outputAmount);
+} 
+ await banking.compound();
+
+  let currentAmount = await banking.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+  button.removeAttribute("disabled");
+});
+
+async function update() {
+  const currentAmount = await banking.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+}
